620ea776562170a169381818b039a5820ec5af2a
marcel.sohnchen <marcel.sohnchen@i07184.DSV.COM>
2020-01-13 10:56:01 +0100

    added "qty on hand" for the location

--- Pickfaces_last_picks.sql
+++ Pickfaces_last_picks.sql
@@ -2,7 +2,8 @@ select pf.CLIENT_ID, pf.SKU_ID, pf.LOCATION_ID,
     case
         when it."LAST PICK" is null then '> 3 months'
         else TO_CHAR(it."LAST PICK", 'DD-MM-YYYY')
-    end "LAST PICK"
+    end "LAST PICK",
+    inv.QTY_ON_HAND
 from(
     select CLIENT_ID, SKU_ID, LOCATION_ID from V_PICK_FACE
 ) pf
@@ -12,6 +13,11 @@ left join
     group by FROM_LOC_ID, CLIENT_ID, SKU_ID 
 ) it
 on pf.CLIENT_ID = it.CLIENT_ID and pf.SKU_ID = it.SKU_ID and pf.LOCATION_ID = it.FROM_LOC_ID
+left join
+(
+    select i.QTY_ON_HAND, i.LOCATION_ID from V_Inventory i
+) inv
+on pf.LOCATION_ID = inv.LOCATION_ID
 order by pf.location_id
 
 
